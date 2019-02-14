071f673c1f0d58738457e8e9450b8cf7dd6c0740
Nikta Serba <nikitaserba@icloud.com>
2019-02-14 22:23:29 +0200

    Update drug class

--- instant/src/main/java/koni/drugger/instant/objects/Drug.kt
+++ instant/src/main/java/koni/drugger/instant/objects/Drug.kt
@@ -1,5 +1,32 @@
 package koni.drugger.instant.objects
 
-import java.util.Date
+import java.util.*
 
-data class Drug (val drugName: String, val drugID: Int, val dateAndTime: Date, val dose: Int , val doseUnit: String?)
+data class Drug (val drugName: String, val drugID: Int, val dateAndTime: Array<Date>, val dose: Int , val doseUnit: String?, val repeat: String?) // repeat - number + h/d/w/m/y (hour, day, week, month, year)
+{
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Drug
+
+        if (drugName != other.drugName) return false
+        if (drugID != other.drugID) return false
+        if (!Arrays.equals(dateAndTime, other.dateAndTime)) return false
+        if (dose != other.dose) return false
+        if (doseUnit != other.doseUnit) return false
+        if (repeat != other.repeat) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = drugName.hashCode()
+        result = 31 * result + drugID
+        result = 31 * result + Arrays.hashCode(dateAndTime)
+        result = 31 * result + dose
+        result = 31 * result + (doseUnit?.hashCode() ?: 0)
+        result = 31 * result + (repeat?.hashCode() ?: 0)
+        return result
+    }
+}
