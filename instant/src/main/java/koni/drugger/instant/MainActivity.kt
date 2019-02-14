051d9df6af566d68db760b9746d8b5e27d17acc5
Grusha5480 <MrGraphitem@gmail.com>
2019-02-14 11:11:17 +0200

    init commit

---
+++ instant/src/main/java/koni/drugger/instant/MainActivity.kt
@@ -0,0 +1,32 @@
+package koni.drugger.instant
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.NavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                this.setTitle(R.string.title_home)
+//                title.(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_add_trip -> {
+                this.setTitle(R.string.title_add_trip)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
