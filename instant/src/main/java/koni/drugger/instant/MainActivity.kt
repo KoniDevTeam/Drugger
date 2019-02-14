83c26992e8da17745072a944d8997d9c4f3424d7
Grusha5480 <MrGraphitem@gmail.com>
2019-02-14 14:27:48 +0200

    Redesigned bottom navigation and add trip button

--- instant/src/main/java/koni/drugger/instant/MainActivity.kt
+++ instant/src/main/java/koni/drugger/instant/MainActivity.kt
@@ -4,19 +4,37 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.NavigationView
 import android.support.v7.app.AppCompatActivity
+import android.text.Layout
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+//    private fun changeView(view: LinearLayout, targetView: Any): Unit {
+//        var _view = view
+//        _view = targetView as LinearLayout;
+//    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
                 this.setTitle(R.string.title_home)
-//                title.(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_add_trip -> {
-                this.setTitle(R.string.title_add_trip)
+            R.id.navigation_drug_wiki -> {
+                this.setTitle(R.string.title_drug_wiki)
+//                changeView(innerView, findViewById(R.id.view_add))
+//                this.innerView = findViewById()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_history -> {
+                this.setTitle(R.string.title_history)
+
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_forum -> {
+                this.setTitle(R.string.title_forum)
+
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -24,9 +42,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+//        var inner = innerView
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//        inner = findViewById(R.id.view_home)
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
