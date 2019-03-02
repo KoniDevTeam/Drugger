13be69c22a5d990b5bd3c6d7f649bb8c7cc512e3
Nikta Serba <nikitaserba@icloud.com>
2019-03-02 22:01:21 +0200

    Fix drug class

--- instant/src/main/java/koni/drugger/instant/objects/PlannedTrip.kt
+++ instant/src/main/java/koni/drugger/instant/objects/PlannedTrip.kt
@@ -2,15 +2,14 @@ package koni.drugger.instant.objects
 
 import java.util.*
 
-data class Drug (val drugName: String, val drugID: Int, val dateAndTime: Array<Date>, val dose: Double , val doseUnit: String?, val repeat: String?) // repeat - number + h/d/w/m/y (hour, day, week, month, year)
+data class PlannedTrip (val drugID: Int, val dateAndTime: Array<Date>, val dose: Double, val doseUnit: String?, val repeat: String?) // repeat - number + h/d/w/m/y (hour, day, week, month, year)
 {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
-        other as Drug
+        other as PlannedTrip
 
-        if (drugName != other.drugName) return false
         if (drugID != other.drugID) return false
         if (!Arrays.equals(dateAndTime, other.dateAndTime)) return false
         if (dose != other.dose) return false
@@ -21,8 +20,7 @@ data class Drug (val drugName: String, val drugID: Int, val dateAndTime: Array<D
     }
 
     override fun hashCode(): Int {
-        var result = drugName.hashCode()
-        result = 31 * result + drugID
+        var result = drugID
         result = 31 * result + Arrays.hashCode(dateAndTime)
         result = 31 * result + dose.hashCode()
         result = 31 * result + (doseUnit?.hashCode() ?: 0)
