ee9f4aaffbb1cd18ef4758b63b7bcc1a9b9653c7
Grusha5480 <MrGraphitem@gmail.com>
2019-03-02 20:34:47 +0200

    code clean

--- instant/src/main/java/koni/drugger/instant/AddTripActivity.kt
+++ instant/src/main/java/koni/drugger/instant/AddTripActivity.kt
@@ -3,6 +3,7 @@ package koni.drugger.instant
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import koni.drugger.instant.dbapi.getAllDrugs
 import kotlinx.android.synthetic.main.activity_add.*
 
 
@@ -12,7 +13,13 @@ class AddTripActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
         this.setTitle(R.string.title_add_trip)
-        print("LOL")
-        this.textView.text = "lol"
+//        val drugs = getAllDrugs(this)
+//        drugs.forEachIndexed { index, drugType ->
+//            println("hello")
+//            if (index.rem(2) == 0){
+//                println("1")
+//            } else
+//                println("2")
+//        }
     }
 }
